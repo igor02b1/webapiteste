e87111adbfdf3e85eef5b1b501e05264cec52d18
Igor de Lima <igor02b1@gmail.com>
2025-04-24 00:20:46 -0300

    Instalando dependencias e adicionando tabelas via code first

---
+++ webapiteste/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace webapiteste.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? nome { get; set; }
+        public string? descricao { get; set; }
+        public decimal? preco  { get; set; }
+        public string? ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+    }
+}
