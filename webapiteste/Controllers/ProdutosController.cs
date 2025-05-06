ba23c861bbe42f006003d381f803a9610d0e58c6
Igor de Lima <igor02b1@gmail.com>
2025-05-06 00:53:04 -0300

    populando as tabelas com dados iniciais e criando rota get e get por id

---
+++ webapiteste/Controllers/ProdutosController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapiteste.Context;
+using webapiteste.Models;
+
+namespace webapiteste.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] 
+        public ActionResult<IEnumerable<Produto>>Get()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            if(produtos is null)
+            {
+                return NotFound("Produtos não encontrados...");
+            }
+
+            return Ok(produtos);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> GetPorID(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+
+            return produto;
+        }
+    }
+}
