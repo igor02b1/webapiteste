042453848b09afbe5a9a804b7016d06792cb54ea
Igor de Lima <igor02b1@gmail.com>
2025-05-07 00:20:34 -0300

    criando as métodos POST, PUT, DELETE

--- webapiteste/Controllers/ProdutosController.cs
+++ webapiteste/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using webapiteste.Context;
 using webapiteste.Models;
 
@@ -29,7 +30,7 @@ namespace webapiteste.Controllers
             return Ok(produtos);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="ObterProduto")]
         public ActionResult<Produto> GetPorID(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
@@ -40,6 +41,50 @@ namespace webapiteste.Controllers
             }
 
             return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if(produto is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Produto produto)
+        {
+            if(id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.Find(id);
+
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
     }
 }
