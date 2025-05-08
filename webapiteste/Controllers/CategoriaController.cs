6db9206fff29c0952a50eea57f210ae8a2ecce7a
Igor de Lima <igor02b1@gmail.com>
2025-05-07 23:58:24 -0300

    Criando crud de categorias.

---
+++ webapiteste/Controllers/CategoriaController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webapiteste.Context;
+using webapiteste.Models;
+
+namespace webapiteste.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categoria = _context.Categorias.ToList();
+            if(categoria is null)
+            {
+                return NotFound("Nenhuma categoria encontrada...");
+            }
+
+            return Ok(categoria);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+        }
+
+        [HttpGet("{id:int}", Name = "ObterCategoria") ]
+        public ActionResult<Categoria> GetPorId(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+            if(categoria is null)
+            {
+                return NotFound("Nenhuma categoria encontrada...");
+            }
+
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public ActionResult<Categoria> Post(Categoria categoria)
+        {
+            if(categoria is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<Categoria> Put(Categoria categoria, int id)
+        {
+            if(categoria.CategoriaId != id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<Categoria> Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+            if(categoria is null)
+            {
+                return NotFound("Categoria não encontrada...");
+            }
+
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+    }
+}
